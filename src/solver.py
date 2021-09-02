9f7c68ffd9c9ba70232393e806a45f16b71e2038
Shmuli02 <samuli.kaleva@gmail.com>
2021-09-02 08:17:46 +0000

    update solver

--- src/solver.py
+++ src/solver.py
@@ -1,6 +1,13 @@
 from os import listdir, read
 from os.path import isfile, join
 from fillapix import FillaPix
+from fillapixprinter import FillaPixPainter
+try:
+    from result_to_excel import FillaPixExcel
+except ModuleNotFoundError:
+    pass
+
+
 try:
     mypath = 'games'
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -19,7 +26,6 @@ while True:
 
         try:
             file_url = mypath + '/' + onlyfiles[int(open_file_n)]
-            print(file_url)
             file = open(file_url,"r")
             game_data = []
             for line in file:
@@ -32,8 +38,15 @@ while True:
                 if box == ';':
                     game_y += 1
             game_x = len(game_data)
-            print(game_x,game_y)
             game = FillaPix(game_x,game_y,game_data)
-            game.main()
-        except:
+            result = game.main()
+            printer_result_turtle = input('Tulostetaanko tulos (turtle) avulla? k/e: ')
+            if printer_result_turtle == 'k':
+                printer = FillaPixPainter(game_x,result)
+                printer.draw()
+            printer_result_excel = input('Tulostetaanko tulos (excel) avulla? k/e: ')
+            if printer_result_excel == 'k':
+                to_excel = FillaPixExcel(onlyfiles[int(open_file_n)],result)
+                to_excel.to_excel()
+        except IndexError:
             print('Väärä syöte')
